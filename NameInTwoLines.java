3fa6c48fb8b8c5522201158ab7e1f9592de728a4
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-14 23:45:35 +0530

    Add files via upload

---
+++ NameInTwoLines.java
@@ -0,0 +1,7 @@
+class NameInTwoLines {
+    public static void main(String[] args) {
+         System.out.println("Rishabh\nRaj");
+         System.out.println("\nCode Stuffs\n");
+         System.out.println("Hyderabad");
+    }
+}
